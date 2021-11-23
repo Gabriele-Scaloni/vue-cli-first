583a20351dc02eb4ccc276fb490acee03ed0a2da
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-11-23 16:58:08 +0100

    creato app.vue

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <ciao msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="La mia prima app con Vue CLI!" />
   </div>
 </template>
 
